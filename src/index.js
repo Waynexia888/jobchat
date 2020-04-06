fd6d13aef689ccb03e5888689425e4e3f7688d5d
Waynexia888 <qiwuwaynexia@gmail.com>
2020-04-06 01:32:40 -0700

    updated

--- src/index.js
+++ src/index.js
@@ -5,6 +5,15 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import { counter } from './index.redux'
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+function Erying(){
+  return <h2>二营</h2>
+}
+
+function Qibinglian(){
+  return <h2>骑兵连</h2>
+}
 
 const store = createStore(
   counter,
@@ -19,10 +28,21 @@ const store = createStore(
 
 
 ReactDOM.render(
-  (<Provider store={store}>
-    <App />
-  </Provider>),
-  document.getElementById('root')
+  <Provider store={store}>
+    <BrowserRouter>
+      <div>
+        <ul>
+          <li><Link to='/'>一营</Link></li>
+          <li><Link to='/erying'>二营</Link></li>
+          <li><Link to='/qibinglian'>骑兵连</Link></li>
+        </ul>
+        <Route exact path='/' component={App}></Route>
+        <Route path='/erying' component={Erying}></Route>
+        <Route path='/qibinglian' component={Qibinglian}></Route>
+      </div>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
 );
 
 
