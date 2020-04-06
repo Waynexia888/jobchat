dd3749c8e0d10fc7ec2fe3429781ec28a9ab1b80
Waynexia888 <qiwuwaynexia@gmail.com>
2020-04-06 02:18:35 -0700

    updated

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import { counter } from './index.redux'
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Link, Redirect, Switch } from 'react-router-dom';
 
 function Erying(){
   return <h2>二营</h2>
@@ -13,6 +13,15 @@ function Erying(){
 
 function Qibinglian(){
   return <h2>骑兵连</h2>
+}
+
+class Test extends React.Component{
+  render(){
+    console.log(this.props)
+    // this.props.history.push('/'); //直接跳转'/'路径
+
+    return <h2>测试组件 {this.props.match.params.location}</h2>
+  }
 }
 
 const store = createStore(
@@ -32,13 +41,23 @@ ReactDOM.render(
     <BrowserRouter>
       <div>
         <ul>
-          <li><Link to='/'>一营</Link></li>
-          <li><Link to='/erying'>二营</Link></li>
-          <li><Link to='/qibinglian'>骑兵连</Link></li>
+          <li>
+            <Link to="/">一营</Link>
+          </li>
+          <li>
+            <Link to="/erying">二营</Link>
+          </li>
+          <li>
+            <Link to="/qibinglian">骑兵连</Link>
+          </li>
         </ul>
-        <Route exact path='/' component={App}></Route>
-        <Route path='/erying' component={Erying}></Route>
-        <Route path='/qibinglian' component={Qibinglian}></Route>
+        {/* <Redirect to='/qibinglian'></Redirect> */}
+        <Switch>
+          <Route exact path="/" component={App}></Route>
+          <Route path="/erying" component={Erying}></Route>
+          <Route path="/qibinglian" component={Qibinglian}></Route>
+          <Route path="/:location" compoment={Test}></Route>
+        </Switch>
       </div>
     </BrowserRouter>
   </Provider>,
