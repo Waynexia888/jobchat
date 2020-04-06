688ec287dd81e9b083bed1051649965be921789e
Waynexia888 <qiwuwaynexia@gmail.com>
2020-04-06 01:02:55 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -2,6 +2,20 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { addGun, removeGun, addGunAsync } from './index.redux';
 
+// const mapStatetoProps = (state) => {
+//   return { num: state };
+// };
+
+// const actionCreators = { addGun, removeGun, addGunAsync };
+// App = connect(mapStatetoProps, actionCreators)(App);
+// 上面代码 相当于如下：
+@connect(
+  //你要state什么属性放到props里
+  state => ({num: state}),
+  //你要什么方法，放到props里，自动dispatch
+  { addGun, removeGun, addGunAsync }
+)
+
 
 class App extends Component {
   render() {
@@ -16,10 +30,5 @@ class App extends Component {
   }
 }
 
-const mapStatetoProps = (state) => {
-  return {num: state}
-}
 
-const actionCreators = { addGun, removeGun, addGunAsync }
-App = connect(mapStatetoProps, actionCreators)(App)
 export default App;
