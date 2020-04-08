563fb9829f3190826f66fcad90d34440b2301dd2
Waynexia888 <qiwuwaynexia@gmail.com>
2020-04-08 03:00:22 -0700

    updated

--- src/Auth.js
+++ src/Auth.js
@@ -1,33 +1,33 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { login } from './Auth.redux';
+import { login, getUserData } from './Auth.redux';
 import { Redirect } from 'react-router-dom';
-import axios from 'axios';
 
 @connect(
     state => state.auth,
-    { login }
+    { login, getUserData }
 )
 class Auth extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-           data: {} 
-        }
-    }
+    // constructor(props){
+    //     super(props);
+    //     this.state = {
+    //        data: {} 
+    //     }
+    // }
     componentDidMount(){
-        axios.get('/data')
-            .then(res => {
+        this.props.getUserData()
+        // axios.get('/data')
+        //     .then(res => {
                 
-                if (res.status === 200) {
-                    this.setState({data: res.data})
-                }
-            })
+        //         if (res.status === 200) {
+        //             this.setState({data: res.data})
+        //         }
+        //     })
     }
     render() {
         return (
             <div>
-                <h2>我的名字是{this.state.data.user}</h2>
+                <h2>我的名字是{this.props.user}, 年龄{this.props.age}</h2>
                 { this.props.isAuth ? <Redirect to='/dashboard' /> : null}
                 <h2>你没有权限，需要登录才能看</h2>
                 <button onClick={this.props.login}>Login</button>
